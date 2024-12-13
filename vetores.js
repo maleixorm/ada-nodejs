6318621efb8cc4a3abe42e3aa1e66575fbf02aae
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-12-13 17:38:02 -0300

    Vetores e Escopo

---
+++ vetores.js
@@ -0,0 +1,9 @@
+const numerosPrimos = [1, 2, 3, 5, 7];
+ {
+    const nomeDeAlunos = ['Joana', 'Cássia', 'Kevin'];
+    console.log(nomeDeAlunos);
+    {
+        const condicoesDeTempo = ['ensolarado', 'nublado', 'chuvoso'];
+        console.log(condicoesDeTempo);
+    }
+ }
