7fe2f14fbecf2066ea10f393751e79498e78fa94
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-12-13 17:20:37 -0300

    Operações com variáveis

---
+++ arquivo2.js
@@ -0,0 +1,16 @@
+const numero1 = 30;
+const numero2 = 10;
+
+const soma = numero1 + numero2;
+const subtracao = numero1 - numero2;
+const multiplicacao = numero1 * numero2;
+const divisao = numero1 / numero2;
+const elevado = numero1 ** numero2;
+const restoDaDivisao = numero1 % numero2;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(elevado);
+console.log(restoDaDivisao);
