c754196800adf1e32308e76e0a23f4d1044ce848
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-12-13 17:20:15 -0300

    Variáveis

---
+++ arquivo.js
@@ -0,0 +1,12 @@
+let numero = 20;
+console.log(numero);
+
+numero = 8;
+console.log(numero);
+
+const numero2 = 300;
+const numeroFavorito = 5.000001;
+const estaChovendo = false;
+
+const nome = 'Marcos';
+console.log(`Oi, ${nome}! Seu numero favorito é: ${numeroFavorito}`);
