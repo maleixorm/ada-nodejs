3db1b22e5a0bb9012f32171c3352a11cf5a0e79d
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2024-12-17 18:05:11 -0300

    Condicionais

---
+++ controle_de_fluxo.js
@@ -0,0 +1,10 @@
+const numero1 = 100;
+const numero2 = 20;
+
+if (numero1 > 1) {
+    console.log('Este número é maior que 1');
+}
+
+if (numero1 > 200) {
+    console.log('Este número é maior que 200');
+}
