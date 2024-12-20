2c826ac2189b2080e5a937ad46d1bda4624c4877
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2024-12-20 00:25:39 -0300

    If and Else on JS

--- controle_de_fluxo.js
+++ controle_de_fluxo.js
@@ -6,7 +6,11 @@ if (numero1 > 1) {
 }
 
 if (numero1 > 200) {
-    console.log('Este número é maior que 200');
+    console.log('Este número é maior que 200!');
 } else {
-    console.log('Este número é menor que 200');
+    console.log('Este número é menor ou igual a 200!');
+}
+
+if (numero2 <= numero1) {
+    console.log(`${numero2} é menor ou igual a ${numero1}!`);
 }
