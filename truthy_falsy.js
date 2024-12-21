fd9c95fd8e8f00463804706344a5124760f870fb
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2024-12-21 14:28:29 -0300

    Truthy and Falsy

---
+++ truthy_falsy.js
@@ -0,0 +1,7 @@
+console.log(!![], !!{}, !!1, !!'asfasd', true);
+
+if ([] && {} && 1 && 'asfasd' && true) {
+    console.log('tudo é verdadeiro.');
+}
+
+console.log(!!null, !!undefined, !!0, !!'', !!NaN);
