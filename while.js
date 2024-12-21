ba508f4447bbbcb4d6a1d15eefde08afbe1f6321
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2024-12-21 16:40:27 -0300

    Utilizando o comando while

---
+++ while.js
@@ -0,0 +1,6 @@
+const nomesDeUsuarios = ['Carla', 'Joana', 'Renata', 'Cleber', 'Zeca'];
+let indice = 0;
+while (indice < nomesDeUsuarios.length) {
+    console.log(`Olá ${nomesDeUsuarios[indice]}`);
+    indice++;
+}
