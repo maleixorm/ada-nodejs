4d58a754c9985a3808ea6baf5d23b1533c1c20c9
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-12-22 18:18:53 -0300

    Trabalhando com o For para Arrays

---
+++ for_of.js
@@ -0,0 +1,14 @@
+const listaDeNumeros = [33, 131, 155, 555, 222, -21, -56];
+const usuarios = ['José', 'Antônio', 'Ramóm'];
+
+// for (let index = 0; index < listaDeNumeros.length; index++) {
+//    console.log(listaDeNumeros[index]);    
+// }
+
+for (const numero of listaDeNumeros) {
+    console.log(numero);
+}
+
+for (const usuario of usuarios) {
+    console.log(usuario);
+}
