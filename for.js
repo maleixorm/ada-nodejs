2439f7edd5ea0409a4fe91203d4cedc39f3ec6b6
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-12-22 18:18:36 -0300

    Trabalhando com o For

---
+++ for.js
@@ -0,0 +1,9 @@
+const listaDeNumeros = [33, 131, 155, 555, 222, -21, -56];
+
+for (let index = 0; index < listaDeNumeros.length; index++) {
+   console.log(listaDeNumeros[index]);    
+}
+
+for (let contador = 0; contador < 100; contador++) {
+    console.log(contador);
+}
