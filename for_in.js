a3aeeee90ee932ececafc0edec056eb7a050211f
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-12-22 19:03:04 -0300

    Tratamento para os nomes dos objetos

--- for_in.js
+++ for_in.js
@@ -6,7 +6,18 @@ const user = {
 };
 
 for (const key in user) {
-    if (key === 'name' || key === 'mail' || key === 'address') {
+    if (key === 'name') {
+        const names = user[key].split(' ');
+        user[key] = '';
+        for (const name of names) {
+            const normalizedName = name.toLowerCase();
+            const [primeiraLetra, ...restoDoNome] = normalizedName;
+            user[key] = user[key] + ' ' + primeiraLetra.toUpperCase() + restoDoNome.join('');
+            user[key] = user[key].trim();
+        }
+    }
+    
+    if (key === 'mail' || key === 'address') {
         user[key] = user[key].toLowerCase();
     }
 }
