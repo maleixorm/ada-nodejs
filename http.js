47c578b4a9e80fae387c727a6733aad075e2f1a1
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-12-29 23:23:14 -0300

    Criando um servidor local para o node

--- http.js
+++ http.js
@@ -1,1 +1,14 @@
 const http = require('http');
+
+const server = http.createServer((request, response) => {
+    const { method, statusCode, url } = request;
+
+    if (url === '/') {
+        response.write(`<div><h1>Hello from Node!</h1><p>http server</p></div>`);
+        response.end();
+    }
+});
+
+server.listen(5000, 'localhost', () => {
+    console.log('Server running on address: http://localhost:5000');
+});
