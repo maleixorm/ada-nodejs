5977b8ddc4a7b127978380104ee1e830dfd97633
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-12-29 23:01:17 -0300

    Trabalhando com DNS

--- dns.js
+++ dns.js
@@ -1,7 +1,5 @@
 const dns = require('node:dns');
 
-const searchedUrl = 'google.com';
-
 // dns.resolve4(searchedUrl, (error, addresses) => {
 //     if (error) {
 //         console.log('URL não encontrada.');
@@ -11,11 +9,28 @@ const searchedUrl = 'google.com';
 // });
 
 async function bootstrap() {
+    const searchedUrl = 'google.com';
+    console.time('Pesquisando URL por DNS padrão');
     const addresses = await dns.promises.resolve4(searchedUrl);
+    console.timeEnd('Pesquisando URL por DNS padrão');
     console.log(addresses);
 
     const nameServers = await dns.promises.resolveNs(searchedUrl);
     console.log(nameServers);
+    
+    const ipNs = await dns.promises.resolve4(nameServers[1]);
+    
+    const resolver = new dns.Resolver();
+    resolver.setServers(ipNs);
+    
+    console.time('Pesquisando URL por DNS específico');
+    resolver.resolve4(searchedUrl, (error, addresses) => {
+        if (error) {
+            console.log('Não foi possível encontrar o IPV4.');
+        }
+        console.timeEnd('Pesquisando URL por DNS específico');
+        console.log(addresses);
+    });
 }
 
 bootstrap();
