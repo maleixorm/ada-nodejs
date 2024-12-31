260a7df4781827e1d3e76fa1737d87702c87c17f
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-12-31 17:39:23 -0300

    criando os métodos para ler e adicionar dados via api

--- http.js
+++ http.js
@@ -1,8 +1,20 @@
 const http = require('http');
+const sports = ['soccer', 'volley', 'basketball', 'tennis'];
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
     const { method, statusCode, url } = request;
-    const sports = ['soccer', 'volley', 'basketball', 'tennis'];
+
+    const bodyPromise = new Promise((resolve, reject) => {
+        let body;
+        
+        request.on('data', data => {
+            body = JSON.parse(data);
+        });
+
+        request.on('end', data => {
+            resolve(body);
+        });
+    });
 
     if (url === '/') {
         response.write(`<div><h1>Hello from Node!</h1><p>http virtual server</p></div>`);
@@ -11,9 +23,22 @@ const server = http.createServer((request, response) => {
     }
 
     if (url === '/api/sports') {
-        response.write(JSON.stringify(sports));
-        response.end();
-        return;
+        if (method === 'GET') {
+            response.write(JSON.stringify(sports));
+            response.end();
+            return;
+        }
+
+        if (method === 'POST') {
+            const body = await bodyPromise;
+            const { name } = body;
+            if (!sports.map(sport => sport.toLowerCase).includes(name.toLowerCase())) {
+                sports.push(name);  
+            }
+            response.write(name.toLowerCase());
+            response.end();
+            return;
+        }
     }
     
     response.statusCode = 404;
